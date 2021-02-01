07afb0d6195c0b240a2587fac5bd5f2248a5326f
eyyzam <bronislawskiandrzej@gmail.com>
2021-02-01 21:23:13 +0100

    Added @ngx-translate to the project dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from "@angular/core";
+import { NgModule, LOCALE_ID } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { RouterModule } from "@angular/router";
+
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { registerLocaleData } from "@angular/common";
+import localeEn from "@angular/common/locales/en";
+
+registerLocaleData(localeEn, "en");
+
+export function HttpLoaderFactory(http: HttpClient) {
+	return new TranslateHttpLoader(http);
+}
 
 @NgModule({
 	declarations: [
@@ -10,9 +23,21 @@ import { AppComponent } from "./app.component";
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		BrowserAnimationsModule,
+		HttpClientModule,
+		AppRoutingModule,
+		RouterModule,
+		TranslateModule.forRoot({
+			loader: {
+				provide: TranslateLoader,
+				useFactory: HttpLoaderFactory,
+				deps: [HttpClient]
+			}
+		})
 	],
-	providers: [],
+	providers: [
+		{ provide: LOCALE_ID, useValue: "pl" }
+	],
 	bootstrap: [AppComponent]
 })
 
