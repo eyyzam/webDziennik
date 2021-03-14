98da401a28a4108a8f4496092ceb8c598fec281b
eyyzam <bronislawskiandrzej@gmail.com>
2021-03-14 19:50:05 +0100

    Added @angular/cdk, few components and imported @angular/material modules

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LayoutRoutingModule } from "./layout-routing.module";
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { SidenavContentComponent } from './sidenav/sidenav-content/sidenav-content.component';
+import { MaterialModule } from "../core/material/material.module";
 
 @NgModule({
-	declarations: [ ],
+	declarations: [ SidenavComponent, SidenavContentComponent],
 	imports: [
 		CommonModule,
-		LayoutRoutingModule
+		LayoutRoutingModule,
+		MaterialModule
 	]
 })
 
