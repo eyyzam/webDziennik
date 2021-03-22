a311596787b14f95a7c28b6c6d5c606efa21f4a6
eyyzam <bronislawskiandrzej@gmail.com>
2021-03-22 22:22:19 +0100

    Added an option to choose between languages in navtop

--- src/app/core/services/common/regular/AppTranslate.service.ts
+++ src/app/core/services/common/regular/AppTranslate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { LanguageKind } from "../../../models";
+import defaultLanguage from "src/assets/i18n/en.json";
 
 @Injectable({
 	providedIn: "root"
@@ -11,20 +12,28 @@ export class AppTranslateService {
 	constructor(private readonly _translateService: TranslateService) { }
 
 	setDefaultLanguage(language: LanguageKind) {
+		this._translateService.setTranslation("en", defaultLanguage);
 		this._translateService.setDefaultLang(language);
 	}
 
 	use(language: string) {
-		if (language)
-			localStorage.setItem("lng", language);
 
-		let languageToUse = this._translateService.getBrowserLang();
-		const localStorageLanguage = localStorage.getItem("lng");
+		try {
+			if (language) {
+				localStorage.setItem("lng", language);
+			}
+			let languageToUse = this._translateService.getBrowserLang();
+			const localStorageLanguage = localStorage.getItem("lng");
 
-		if (localStorageLanguage !== null)
-			languageToUse = localStorageLanguage;
+			if (localStorageLanguage !== null)
+				languageToUse = localStorageLanguage;
 
-		this._translateService.use(languageToUse);
+			this._translateService.use(languageToUse);
+		} catch (e) {
+			language
+				? this._translateService.use(language)
+				: this._translateService.use(this._translateService.getBrowserLang());
+		}
 	}
 
 	get currentLanguage(): LanguageKind {
@@ -33,5 +42,17 @@ export class AppTranslateService {
 
 	get defaultLanguage(): LanguageKind {
 		return this._translateService.defaultLang as LanguageKind;
+	}
+
+	get languagesList(): Array<string> {
+		const languages = new Array<string>();
+
+		Object.values(LanguageKind).forEach(lang => {
+			languages.push(lang);
+		});
+
+		const [, ...languagesArr] = languages; 
+
+		return languagesArr;
 	}
 }
--- src/app/layout/navtop/navtop.component.ts
+++ src/app/layout/navtop/navtop.component.ts
@@ -1,24 +1,37 @@
 import { Component, OnInit } from "@angular/core";
 import { LayoutService } from "../services/layout.service";
+import { AppTranslateService } from "../../core/services/common/regular/AppTranslate.service";
+import { LanguageKind } from "../../core/models/enums/LanguageKind";
 
 @Component({
   selector: "app-navtop",
   templateUrl: "./navtop.component.html",
   styleUrls: ["./navtop.component.less"]
 })
+
 export class NavtopComponent implements OnInit  {
 
 	sidenavOpened!: boolean;
+	languages!: Array<string>;
 
-	constructor(private readonly layoutService: LayoutService) { }
+	constructor(
+		private readonly _layoutService: LayoutService,
+		private readonly _translateService: AppTranslateService
+	) { }
 
 	toggleSidenav() {
-		this.layoutService.toggleSidenav();
+		this._layoutService.toggleSidenav();
 	}
 
 	ngOnInit() {
-		this.layoutService.sidenavOpened().subscribe(sidenavStatus => {
+		this.languages = this._translateService.languagesList;
+
+		this._layoutService.sidenavOpened().subscribe(sidenavStatus => {
 			this.sidenavOpened = sidenavStatus;
 		});
+	}
+
+	changeLanguage(lang: string) {
+		this._translateService.use(lang);
 	}
 }
